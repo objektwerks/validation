8e102054c2b46522fb164505c150cc968fc02fd8
objektwerks <objektwerks@runbox.com>
2020-11-14 07:45:06 -0500

    simplified person validate

--- src/test/scala/validator/Person.scala
+++ src/test/scala/validator/Person.scala
@@ -5,26 +5,17 @@ import scala.util.Try
 final case class Person(name: String, age: Int)
 
 extension (person: Person) {
-  def validateName: Unit = require(person.name.nonEmpty, "Name is empty.")
-  def validateAge: Unit = require(person.age > 0, s"Age of ${person.age} is less than 1.")
+  def validate: Person = {
+    require(person.name.nonEmpty, "Name is empty.")
+    require(person.age > 0, s"Age of ${person.age} is less than 1.")
+    person
+  }
 }
 
 given PersonValidator as Validator[Throwable, Person] {
-  def validate(person: Person): Either[Throwable, Person] =
-    Try {
-      person.validateName
-      person.validateAge
-      Person(person.name, person.age)
-    }.toEither
+  def validate(person: Person): Either[Throwable, Person] = Try( person.validate ).toEither
 }
 
 given PersonValidators as Validators[Throwable, Person] {
-  def validates(persons: Seq[Person]): Seq[Either[Throwable, Person]] =
-    persons.map { person =>
-      Try {
-        person.validateName
-        person.validateAge
-        Person(person.name, person.age)
-      }.toEither
-    }
+  def validates(persons: Seq[Person]): Seq[Either[Throwable, Person]] = persons.map { person => Try( person.validate ).toEither }
 }
