5d5857d9667b0312751046b27f0a415b0d90eaed
objektwerks <objektwerks@runbox.com>
2020-11-14 05:24:32 -0500

    refactored test

--- src/main/scala/validator/Validators.scala
+++ src/main/scala/validator/Validators.scala
@@ -4,5 +4,5 @@ trait Validators[E, T] {
   def validates(entities: Seq[T]): Seq[Either[E, T]]
 }
 
-def validates[E, T](entities: Seq[T])(using validators: Validators[E, T])(using validator: Validator[E, T]): Seq[Either[E, T]] = 
+def validates[E, T](entities: Seq[T])(using validators: Validators[E, T], validator: Validator[E, T]): Seq[Either[E, T]] = 
   entities.map { entity => validator.validate(entity) }
--- src/test/scala/validator/ValidatorTest.scala
+++ src/test/scala/validator/ValidatorTest.scala
@@ -5,9 +5,15 @@ import munit._
 class ValidatorTest extends FunSuite {
   test("validator") {
     val person = Person("Fred Flintsone", 28)
-    validate( person ) match {
-      case Left(error) => fail(error.getMessage)
-      case Right(entity) => assert(entity != null)
-    }
+    val validatedPerson = validate( person )
+    assert( validatedPerson.isRight )
+  }
+
+  test("validators") {
+    val persons = List( Person("Fred Flintsone", 28), Person("Betty Flintsone", 27) )
+    val validatedPersons = validates( persons )
+    assert( validatedPersons.length == 2 )
+    assert( validatedPersons(0).isRight )
+    assert( validatedPersons(1).isRight )
   }
 }
