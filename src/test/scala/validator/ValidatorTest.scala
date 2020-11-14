da0bad8992a347e5e9c357557c35274a05b970d3
objektwerks <objektwerks@runbox.com>
2020-11-14 05:30:03 -0500

    enhanced test

--- src/test/scala/validator/ValidatorTest.scala
+++ src/test/scala/validator/ValidatorTest.scala
@@ -3,17 +3,31 @@ package validator
 import munit._
 
 class ValidatorTest extends FunSuite {
-  test("validator") {
+  test("validator > valid") {
     val person = Person("Fred Flintsone", 28)
     val validatedPerson = validate( person )
     assert( validatedPerson.isRight )
   }
 
-  test("validators") {
+  test("validator > invalid") {
+    val person = Person("", 0)
+    val validatedPerson = validate( person )
+    assert( validatedPerson.isLeft )
+  }
+
+  test("validators > valid") {
     val persons = List( Person("Fred Flintsone", 28), Person("Betty Flintsone", 27) )
     val validatedPersons = validates( persons )
     assert( validatedPersons.length == 2 )
     assert( validatedPersons(0).isRight )
     assert( validatedPersons(1).isRight )
+  }
+
+  test("validators > invalid") {
+    val persons = List( Person("", 0), Person("", 0) )
+    val validatedPersons = validates( persons )
+    assert( validatedPersons.length == 2 )
+    assert( validatedPersons(0).isLeft )
+    assert( validatedPersons(1).isLeft )
   }
 }
