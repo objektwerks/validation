c814263135e565adfbbd554adce7f63844f3afef
objektwerks <objektwerks@runbox.com>
2020-11-14 06:09:12 -0500

    simplified test

--- src/test/scala/validator/ValidatorTest.scala
+++ src/test/scala/validator/ValidatorTest.scala
@@ -18,7 +18,6 @@ class ValidatorTest extends FunSuite {
   test("validators > valid") {
     val persons = List( Person("Fred Flintsone", 28), Person("Betty Flintsone", 27) )
     val validatedPersons = validates( persons )
-    assert( validatedPersons.length == 2 )
     assert( validatedPersons(0).isRight )
     assert( validatedPersons(1).isRight )
   }
@@ -26,7 +25,6 @@ class ValidatorTest extends FunSuite {
   test("validators > invalid") {
     val persons = List( Person("", 0), Person("", 0) )
     val validatedPersons = validates( persons )
-    assert( validatedPersons.length == 2 )
     assert( validatedPersons(0).isLeft )
     assert( validatedPersons(1).isLeft )
   }
