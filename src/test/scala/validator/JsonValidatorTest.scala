1077a63390bacbdeeb2f09c4814b8d4193a1fd70
objektwerks <objektwerks@runbox.com>
2020-11-17 16:42:11 -0500

    reformatted json triple quotes

--- src/test/scala/validator/JsonValidatorTest.scala
+++ src/test/scala/validator/JsonValidatorTest.scala
@@ -6,7 +6,7 @@ class JsonValidatorTest extends FunSuite {
   test("valid") {
     val value = """
                  { "name":"Fred Flintstone", "age": 28 }
-               """
+                """
     val values = Seq( value )
     val json = Json( values )
     val validatedPersons = validateEntity( json )
@@ -16,7 +16,7 @@ class JsonValidatorTest extends FunSuite {
   test("invalid") {
     val value = """
                  { "name":"Fred Flintstone", "age": 0 }
-               """
+                """
     val values = Seq( value )
     val json = Json( values )
     val validatedPersons = validateEntity( json )
