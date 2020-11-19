254e9a42df56f56b5605a590bb40cb80be5a6a91
objektwerks <objektwerks@runbox.com>
2020-11-19 15:01:49 -0500

    refactored csv by adding column, row and rows types

--- src/test/scala/validator/Csv.scala
+++ src/test/scala/validator/Csv.scala
@@ -1,3 +1,12 @@
 package validator
 
-final case class Csv(rows: Seq[Seq[String]])
+type Column = String
+def Column(column: String): Column = column
+
+type Row = Seq[Column]
+def Row(columns: Column*): Seq[Column] = Seq(columns: _*)
+
+type Rows = Seq[Row]
+def Rows(rows: Row*): Seq[Row] = Seq(rows: _*)
+
+final case class Csv(rows: Rows)
--- src/test/scala/validator/CsvValidatorTest.scala
+++ src/test/scala/validator/CsvValidatorTest.scala
@@ -4,16 +4,16 @@ import munit._
 
 class CsvValidatorTest extends FunSuite {
   test("valid") {
-    val columns = Seq( "Fred Flintstone", "28" )
-    val rows = Seq( columns )
+    val row = Row( Column("Fred Flintstone"), Column("28") )
+    val rows = Rows( row )
     val csv = Csv( rows )
     val validatedPerson = validateEntity( csv )
     assert( validatedPerson.isRight )
   }
 
   test("invalid") {
-    val columns = Seq( "Fred Flintstone", "0" )
-    val rows = Seq( columns )
+    val row = Row( Column("Fred Flintstone"), Column("0") )
+    val rows = Rows( row )
     val csv = Csv( rows )
     val validatedPerson = validateEntity( csv )
     assert( validatedPerson.isLeft )
