c9bf41ffb368552b12b282488f854d9bc88b8130
objektwerks <objektwerks@runbox.com>
2020-12-31 08:58:13 -0500

    refactored munit to scalatest

--- src/test/scala/validator/EntityValidatorTest.scala
+++ src/test/scala/validator/EntityValidatorTest.scala
@@ -1,22 +1,23 @@
 package validator
 
-import munit._
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-class EntityValidatorTest extends FunSuite {
+class EntityValidatorTest extends AnyFunSuite with Matchers {
   test("validate") {
     val person = Person("Fred Flintsone", 28)
-    assert( person.validate == person )
+    person.validate shouldBe person
   }
 
   test("valid") {
     val person = Person("Fred Flintsone", 28)
     val validatedPerson = validateEntity( person )
-    assert( validatedPerson.isRight )
+    validatedPerson.isRight shouldBe true
   }
 
   test("invalid") {
     val person = Person("Fred Flintstone", 0)
     val validatedPerson = validateEntity( person )
-    assert( validatedPerson.isLeft )
+    validatedPerson.isLeft shouldBe true
   }
 }
