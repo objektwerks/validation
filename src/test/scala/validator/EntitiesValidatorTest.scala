fcdbe3ea0309921966193e81456d30b8c21db25e
objektwerks <objektwerks@runbox.com>
2020-12-31 08:57:05 -0500

    refactored munit to scalatest

--- src/test/scala/validator/EntitiesValidatorTest.scala
+++ src/test/scala/validator/EntitiesValidatorTest.scala
@@ -1,26 +1,27 @@
 package validator
 
-import munit._
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-class EntitiesValidatorTest extends FunSuite {
+class EntitiesValidatorTest extends AnyFunSuite with Matchers {
   test("valid") {
     val persons = Seq( Person("Fred Flintsone", 28), Person("Betty Flintsone", 27) )
     val validatedPersons = validateEntities( persons )
-    assert( validatedPersons(0).isRight )
-    assert( validatedPersons(1).isRight )
+    validatedPersons(0).isRight shouldBe true
+    validatedPersons(1).isRight shouldBe true
   }
 
   test("invalid") {
     val persons = Seq( Person("Fred Flintstone", 0), Person("", 0) )
     val validatedPersons = validateEntities( persons )
-    assert( validatedPersons(0).isLeft )
-    assert( validatedPersons(1).isLeft )
+    validatedPersons(0).isLeft shouldBe true
+    validatedPersons(1).isLeft shouldBe true
   }
 
   test("valid | invalid") {
     val persons = Seq( Person("Fred Flintstone", 28), Person("Fred Flintstone", 0) )
     val validatedPersons = validateEntities( persons )
-    assert( validatedPersons(0).isRight )
-    assert( validatedPersons(1).isLeft )
+    validatedPersons(0).isRight shouldBe true
+    validatedPersons(1).isLeft shouldBe true
   }
 }
