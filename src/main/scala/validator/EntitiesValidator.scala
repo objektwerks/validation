df09fafee778d677bdb2f085e339ad2c7486ed11
objektwerks <objektwerks@runbox.com>
2023-04-17 18:50:30 -0400

    removed entities validator

--- src/main/scala/validator/EntitiesValidator.scala
+++
@@ -1,8 +0,0 @@
-package validator
-
-trait EntitiesValidator[R, E, T]:
-  def validate(entities: Seq[R]): Seq[Either[E, T]]
-
-def validateEntities[R, E, T](entities: Seq[R])
-  (using entitiesValidator: EntitiesValidator[R, E, T])
-  (using entityValidator: EntityValidator[R, E, T]): Seq[Either[E, T]] = entities.map { entity => entityValidator.validate(entity) }
