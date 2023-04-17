acddff60a454a6f6b8323557da3e2e81b7e91941
objektwerks <objektwerks@runbox.com>
2023-04-17 18:50:08 -0400

    added validators

---
+++ src/main/scala/validator/Validators.scala
@@ -0,0 +1,16 @@
+package validator
+
+object Validators {
+  trait EntityValidator[R, E, T]:
+    def validate(entity: R): Either[E, T]
+
+  def validateEntity[R, E, T](entity: R)
+    (using entityValidator: EntityValidator[R, E, T]): Either[E, T] = entityValidator.validate(entity)
+
+  trait EntitiesValidator[R, E, T]:
+    def validate(entities: Seq[R]): Seq[Either[E, T]]
+
+  def validateEntities[R, E, T](entities: Seq[R])
+    (using entitiesValidator: EntitiesValidator[R, E, T])
+    (using entityValidator: EntityValidator[R, E, T]): Seq[Either[E, T]] = entities.map { entity => entityValidator.validate(entity) }
+}
