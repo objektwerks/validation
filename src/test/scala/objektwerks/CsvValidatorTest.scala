5fa818f984f4db3837b25dcd077a98ba9db5d9dd
Objektwerks <objektwerks@runbox.com>
2023-09-25 10:56:26 -0400

    Update CsvValidatorTest.scala

--- src/test/scala/objektwerks/CsvValidatorTest.scala
+++ src/test/scala/objektwerks/CsvValidatorTest.scala
@@ -5,19 +5,17 @@ import org.scalatest.matchers.should.Matchers
 
 import Validators.*
 
-class CsvValidatorTest extends AnyFunSuite with Matchers:
-  test("valid") {
+final class CsvValidatorTest extends AnyFunSuite with Matchers:
+  test("valid"):
     val row = Row( Column("Fred Flintstone"), Column("28") )
     val rows = Rows( row )
     val csv = Csv( rows )
     val validatedPerson = validateEntity( csv )
     validatedPerson.isRight shouldBe true
-  }
 
-  test("invalid") {
+  test("invalid"):
     val row = Row( Column("Fred Flintstone"), Column("0") )
     val rows = Rows( row )
     val csv = Csv( rows )
     val validatedPerson = validateEntity( csv )
     validatedPerson.isLeft shouldBe true
-  }
