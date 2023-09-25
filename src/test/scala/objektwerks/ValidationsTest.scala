4de36998c91d9348c9948372b7837589740b4858
Objektwerks <objektwerks@runbox.com>
2023-09-25 10:58:30 -0400

    Update ValidationsTest.scala

--- src/test/scala/objektwerks/ValidationsTest.scala
+++ src/test/scala/objektwerks/ValidationsTest.scala
@@ -3,18 +3,16 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class ValidationsTest extends AnyFunSuite with Matchers:
-  test("valid") {
+final class ValidationsTest extends AnyFunSuite with Matchers:
+  test("valid"):
     val person = Person("Fred Flintsone", 28)
     val validations = person.validations
     validations.isValid shouldBe true
-  }
 
-  test("invalid") {
+  test("invalid"):
     val person = Person("", 0)
     val validations = person.validations
     validations.isValid shouldBe false
     validations.get("name").nonEmpty shouldBe true
     validations.get("age").nonEmpty shouldBe true
     validations.collect.size shouldBe 2
-  }
