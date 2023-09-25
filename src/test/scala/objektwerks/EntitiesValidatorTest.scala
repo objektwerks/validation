a626a3e86040ced84de6d9f8e3d814c5ac435cb6
Objektwerks <objektwerks@runbox.com>
2023-09-25 10:56:59 -0400

    Update EntitiesValidatorTest.scala

--- src/test/scala/objektwerks/EntitiesValidatorTest.scala
+++ src/test/scala/objektwerks/EntitiesValidatorTest.scala
@@ -5,24 +5,21 @@ import org.scalatest.matchers.should.Matchers
 
 import Validators.*
 
-class EntitiesValidatorTest extends AnyFunSuite with Matchers:
-  test("valid") {
+final class EntitiesValidatorTest extends AnyFunSuite with Matchers:
+  test("valid"):
     val persons = Seq( Person("Fred Flintsone", 28), Person("Betty Flintsone", 27) )
     val validatedPersons = validateEntities( persons )
     validatedPersons(0).isRight shouldBe true
     validatedPersons(1).isRight shouldBe true
-  }
 
-  test("invalid") {
+  test("invalid"):
     val persons = Seq( Person("Fred Flintstone", 0), Person("", 0) )
     val validatedPersons = validateEntities( persons )
     validatedPersons(0).isLeft shouldBe true
     validatedPersons(1).isLeft shouldBe true
-  }
 
-  test("valid | invalid") {
+  test("valid | invalid"):
     val persons = Seq( Person("Fred Flintstone", 28), Person("Fred Flintstone", 0) )
     val validatedPersons = validateEntities( persons )
     validatedPersons(0).isRight shouldBe true
     validatedPersons(1).isLeft shouldBe true
-  }
