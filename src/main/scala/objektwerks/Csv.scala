1df8466389ba3607fa3d3f4a8442c1f08aa1446f
objektwerks <objektwerks@runbox.com>
2024-09-02 09:45:09 -0400

    corrected scala 3 syntax error

--- src/main/scala/objektwerks/Csv.scala
+++ src/main/scala/objektwerks/Csv.scala
@@ -4,9 +4,9 @@ type Column = String
 def Column(column: String): Column = column
 
 type Row = Seq[Column]
-def Row(columns: Column*): Seq[Column] = Seq(columns: _*)
+def Row(columns: Column*): Seq[Column] = Seq(columns*)
 
 type Rows = Seq[Row]
-def Rows(rows: Row*): Seq[Row] = Seq(rows: _*)
+def Rows(rows: Row*): Seq[Row] = Seq(rows*)
 
 final case class Csv(rows: Rows)
