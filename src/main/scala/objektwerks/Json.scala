87cd6942fe359b92b795802e6611476d7bf033d6
objektwerks <objektwerks@runbox.com>
2024-09-02 09:45:29 -0400

    corrected scala 3 syntax error

--- src/main/scala/objektwerks/Json.scala
+++ src/main/scala/objektwerks/Json.scala
@@ -4,6 +4,6 @@ type JsonObject = String
 def JsonObject(json: String): JsonObject = json
 
 type JsonObjects = Seq[JsonObject]
-def JsonObjects(jsonObjects: JsonObject*): Seq[JsonObject] = Seq(jsonObjects: _*)
+def JsonObjects(jsonObjects: JsonObject*): Seq[JsonObject] = Seq(jsonObjects*)
 
 final case class Json(jsonObjects: JsonObjects)
