1c437730bb2e0da027f925db372fae26c5884bad
objektwerks <objektwerks@runbox.com>
2024-09-05 18:55:38 -0400

    refactored invalidations

--- src/main/scala/objektwerks/Invalidations.scala
+++ src/main/scala/objektwerks/Invalidations.scala
@@ -6,28 +6,28 @@ class Invalidations:
   type Field = String
   type Message = String
 
-  private val invalidFields = mutable.Map[Field, Message]()
+  private val invalidations = mutable.Map[Field, Message]()
 
   private def add(field: Field, message: Message): Invalidations =
-    invalidFields += field -> message
+    invalidations += field -> message
     this
 
-  def isEmpty: Boolean = invalidFields.isEmpty
+  def isEmpty: Boolean = invalidations.isEmpty
 
-  def count: Int = invalidFields.size
+  def count: Int = invalidations.size
 
-  def get(field: Field): Option[Message] = invalidFields.get(field)
+  def get(field: Field): Option[Message] = invalidations.get(field)
 
-  def asList: List[String] = invalidFields.map { (_, message) => s"$message" }.toList
+  def asList: List[String] = invalidations.map { (_, message) => s"$message" }.toList
 
-  def asMap: Map[Field, Message] = invalidFields.toMap
+  def asMap: Map[Field, Message] = invalidations.toMap
 
   def asString: String = asList.mkString(",")
 
-  def invalidate(expression: Boolean)(field: Field, message: Message): Invalidations =
-    if expression then add(field, message)
+  def invalidate(isInvalidExpr: Boolean)(field: Field, message: Message): Invalidations =
+    if isInvalidExpr then add(field, message)
     else this
 
-  def invalidate(fn: () => Boolean)(field: Field, message: Message): Invalidations =
-    if fn() then add(field, message)
+  def invalidate(isInvalidFn: () => Boolean)(field: Field, message: Message): Invalidations =
+    if isInvalidFn() then add(field, message)
     else this
