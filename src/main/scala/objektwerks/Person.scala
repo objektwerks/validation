60ee056708d07734a08fcde44fe42b27386a865f
objektwerks <objektwerks@runbox.com>
2024-09-06 09:44:42 -0400

    moved validator givens from person to validators

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -1,10 +1,5 @@
 package objektwerks
 
-import scala.util.Try
-import scala.collection.mutable.ArrayBuffer
-
-import ujson.*
-
 import Person.*
 import Types.*
 import Validators.*
@@ -28,34 +23,3 @@ extension (person: Person)
     Invalidator()
       .invalidate(person.name.isEmpty)(nameField, nameMessage)
       .invalidate(person.age < 1)(ageField, ageMessage)
-
-given EntityValidator[Person, Throwable, Person] with
-  def validate(person: Person): Either[Throwable, Person] = Try( person.validate ).toEither
-
-given EntitiesValidator[Person, Throwable, Person] with
-  def validate(persons: Seq[Person]): Seq[Either[Throwable, Person]] = persons.map { person => Try( person.validate ).toEither }
-
-given EntityValidator[Csv, Throwable, Seq[Person]] with
-  def validate(csv: Csv): Either[Throwable, Seq[Person]] =
-    Try:
-      val persons = ArrayBuffer[Person]()
-      for row <- csv.rows
-        yield
-          val name = row.head
-          val age = Try( row(1).toInt ).getOrElse(0)
-          persons.addOne( Person(Name(name), Age(age)).validate )
-      persons.toSeq
-    .toEither
-
-given EntityValidator[Json, Throwable, Seq[Person]] with
-  def validate(json: Json): Either[Throwable, Seq[Person]] =
-    Try:
-      val persons = ArrayBuffer[Person]()
-      for jsonObject <- json.jsonObjects
-        yield
-          val jsonValue = ujson.read(jsonObject)
-          val name = jsonValue("name").str
-          val age = jsonValue("age").num.toInt
-          persons.addOne( Person(Name(name), Age(age)).validate )
-      persons.toSeq
-    .toEither
--- src/main/scala/objektwerks/Validators.scala
+++ src/main/scala/objektwerks/Validators.scala
@@ -1,5 +1,12 @@
 package objektwerks
 
+import scala.util.Try
+import scala.collection.mutable.ArrayBuffer
+
+import ujson.*
+
+import Types.*
+
 object Validators:
   trait EntityValidator[R, E, T]:
     def validate(entity: R): Either[E, T]
@@ -13,3 +20,34 @@ object Validators:
   def validateEntities[R, E, T](entities: Seq[R])
     (using entitiesValidator: EntitiesValidator[R, E, T])
     (using entityValidator: EntityValidator[R, E, T]): Seq[Either[E, T]] = entities.map { entity => entityValidator.validate(entity) }
+
+  given EntityValidator[Person, Throwable, Person] with
+    def validate(person: Person): Either[Throwable, Person] = Try( person.validate ).toEither
+
+  given EntitiesValidator[Person, Throwable, Person] with
+    def validate(persons: Seq[Person]): Seq[Either[Throwable, Person]] = persons.map { person => Try( person.validate ).toEither }
+
+  given EntityValidator[Csv, Throwable, Seq[Person]] with
+    def validate(csv: Csv): Either[Throwable, Seq[Person]] =
+      Try:
+        val persons = ArrayBuffer[Person]()
+        for row <- csv.rows
+          yield
+            val name = row.head
+            val age = Try( row(1).toInt ).getOrElse(0)
+            persons.addOne( Person(Name(name), Age(age)).validate )
+        persons.toSeq
+      .toEither
+
+  given EntityValidator[Json, Throwable, Seq[Person]] with
+    def validate(json: Json): Either[Throwable, Seq[Person]] =
+      Try:
+        val persons = ArrayBuffer[Person]()
+        for jsonObject <- json.jsonObjects
+          yield
+            val jsonValue = ujson.read(jsonObject)
+            val name = jsonValue("name").str
+            val age = jsonValue("age").num.toInt
+            persons.addOne( Person(Name(name), Age(age)).validate )
+        persons.toSeq
+      .toEither
