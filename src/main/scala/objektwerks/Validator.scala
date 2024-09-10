5a654ec5eafc91ed16b5b4a5508bbc29a6fd638a
objektwerks <objektwerks@runbox.com>
2024-09-10 12:40:48 -0400

    added validator

---
+++ src/main/scala/objektwerks/Validator.scala
@@ -0,0 +1,36 @@
+package objektwerks
+
+import scala.collection.mutable
+
+import Types.*
+
+final class Validator:
+  private val invalidations = mutable.Map[Field, Message]()
+
+  private def add(field: Field, message: Message): Validator =
+    invalidations += field -> message
+    this
+
+  def isValid: Boolean = invalidations.isEmpty
+
+  def isInvalid: Boolean = !invalidations.isEmpty
+
+  def count: Int = invalidations.size
+
+  def asList: List[String] = invalidations.map { (field, message) => s"[$field] $message" }.toList
+
+  def asMap: Map[Field, Message] = invalidations.toMap
+
+  def asString(separator: String = ","): String = asList.mkString(separator)
+
+  def validate(expr: Boolean)(field: Field, message: Message): Validator =
+    if expr then this
+    else add(field, message)
+
+  def validate(fn: () => Boolean)(field: Field, message: Message): Validator =
+    if fn() then this
+    else add(field, message)
+
+  def validate(validator: Validator): Validator =
+    invalidations ++= validator.asMap
+    this
