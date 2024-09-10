9b0f9774f5c1227eb4272767bb4b219767f9f45d
objektwerks <objektwerks@runbox.com>
2024-09-10 13:24:19 -0400

    enhanced name and age types

--- src/main/scala/objektwerks/Types.scala
+++ src/main/scala/objektwerks/Types.scala
@@ -4,10 +4,14 @@ object Types:
   opaque type Name <: String = String
   object Name:
     def apply(value: String): Name = value
+    extension (name: Name)
+      def value: String = name
 
   opaque type Age <: Int = Int
   object Age:
     def apply(value: Int): Age = value
+    extension (age: Age)
+      def value: Int = age
 
   opaque type Field = String
   object Field:
