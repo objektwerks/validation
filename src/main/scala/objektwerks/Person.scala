070524c4710189cb7fa17b8c64642a43e7327ff5
objektwerks <objektwerks@runbox.com>
2024-09-10 13:18:29 -0400

    edited person

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -8,8 +8,8 @@ object Person:
   val nameField = Field("Name")
   val ageField = Field("Age")
 
-  val nameMessage = Message("Is less than 1 character.")
-  val ageMessage = Message("Is less than 1.")
+  val nameMessage = Message("is less than 1 character.")
+  val ageMessage = Message("is less than 1.")
 
 final case class Person(name: Name, age: Age)
 
@@ -26,5 +26,5 @@ extension (person: Person)
 
   def xvalidate: Validator =
     Validator()
-      .validate(person.name.nonEmpty)(nameField, Message("Must be non empty."))
-      .validate(person.age > 0)(ageField, Message("Must be greater than 1."))
+      .validate(person.name.nonEmpty)(nameField, Message("must be non empty."))
+      .validate(person.age > 0)(ageField, Message("must be greater than 1."))
